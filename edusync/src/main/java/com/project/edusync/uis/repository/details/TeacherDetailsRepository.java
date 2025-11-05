d3c49f24d2065c9bc4b4ea43d9eeb0ca3b01636c
sarthakyadav25 <sarthakvyadav@gmail.com>
2025-11-06 00:58:19 +0530

    Added: schedule management flow for manual allocation of timetables

--- edusync/src/main/java/com/project/edusync/uis/repository/details/TeacherDetailsRepository.java
+++ edusync/src/main/java/com/project/edusync/uis/repository/details/TeacherDetailsRepository.java
@@ -1,7 +1,15 @@
 package com.project.edusync.uis.repository.details;
 
+import com.project.edusync.adm.model.entity.Room;
 import com.project.edusync.uis.model.entity.details.TeacherDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TeacherDetailsRepository extends JpaRepository<TeacherDetails, Integer> {
+
+    @Query("SELECT t FROM TeacherDetails t WHERE t.id = :teacherId")
+    Optional<TeacherDetails> findActiveById(Long teacherId);
 }
