dd712ac8f1d7414faa4f9393b0adc11c02ca9e6d
Singh <singh.ashishhhh@gmail.com>
2025-11-06 14:11:15 +0530

    Added: Added Query required for BulkImportService

--- edusync/src/main/java/com/project/edusync/adm/repository/SectionRepository.java
+++ edusync/src/main/java/com/project/edusync/adm/repository/SectionRepository.java
@@ -4,9 +4,13 @@ import com.project.edusync.adm.model.entity.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
     @Query("SELECT s FROM Section s JOIN s.academicClass ac WHERE ac.name = :className AND s.sectionName = :sectionName")
     Optional<Section> findByAcademicClass_NameAndSectionName(String className, String sectionName);
+
+    @Query("SELECT s FROM Section s JOIN FETCH s.academicClass")
+    List<Section> findAllWithClass();
 }
