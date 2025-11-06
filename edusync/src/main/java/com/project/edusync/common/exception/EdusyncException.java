852935034c5a32145dbb8255d617347db2aafd2c
Singh <singh.ashishhhh@gmail.com>
2025-11-06 12:08:52 +0530

    Added: Added Base Exception class

--- edusync/src/main/java/com/project/edusync/common/exception/EdusyncException.java
+++ edusync/src/main/java/com/project/edusync/common/exception/EdusyncException.java
@@ -2,6 +2,7 @@ package com.project.edusync.common.exception;
 
 public class EdusyncException extends RuntimeException {
     public EdusyncException(String message) {
+        //This is the base exception
         super(message);
     }
 }
