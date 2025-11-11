f05382813eae1fa6cd9f695ebdd409d268cac58d
SagarBokade <sagarnbokade@gmail.com>
2025-11-12 01:09:59 +0530

    Added: Added some api and servic classes in ams module

--- edusync/src/main/java/com/project/edusync/ams/model/controller/AttendanceTypeController.java
+++ edusync/src/main/java/com/project/edusync/ams/model/controller/AttendanceTypeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/ams/types")
+@RequestMapping("${api.url}/auth/ams/types")
 @RequiredArgsConstructor
 public class AttendanceTypeController {
 
