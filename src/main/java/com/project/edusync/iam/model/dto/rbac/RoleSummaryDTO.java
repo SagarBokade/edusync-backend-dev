c83eb730c765640ebf317695bcd92d4c3b6f2d20
Ashish Ranjan Singh <71336173+ashugolem@users.noreply.github.com>
2026-04-01 14:05:23 +0530

    refactor(iam): enhance RBAC management and deprecate AOP request logging

--- src/main/java/com/project/edusync/iam/model/dto/rbac/RoleSummaryDTO.java
+++ src/main/java/com/project/edusync/iam/model/dto/rbac/RoleSummaryDTO.java
@@ -1,9 +1,8 @@
 package com.project.edusync.iam.model.dto.rbac;
 
 public record RoleSummaryDTO(
-        Integer id,
-        String name,
-        boolean active
+		Integer id,
+		String name
 ) {
 }
 
